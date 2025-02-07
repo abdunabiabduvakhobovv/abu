233fce1cd4a65c9740934f1909565c1439346774
abdunabiabduvakhobovv <abdunabiabduvakhobov60@gmail.com>
2025-02-07 15:48:23 +0500

    Add files via upload

---
+++ javaskript 2/main.js
@@ -0,0 +1,13 @@
+console.log(typeof "sbhbfhebf");
+console.log(typeof 123);
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof 545457n);
+console.log(typeof Symbol("sdf"));
+ 
+
+
+
+
+
